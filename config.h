10c4133593ad0c77bcb57f1b623594768631148d
Cláudio Torres Resende <86234175+IGN-Styly@users.noreply.github.com>
2022-10-13 19:49:05 -0300

    Update config.h

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+		/*Does not curently work with BG IMG*/
 };
 
 /* treat a cleared input like a wrong password (color) */
